09a3faf797e7c4a103ed9b33701f20def220e1bd
Bas van Hoewijk <basvanhoewijk@gmail.com>
2018-12-10 20:56:05 +0100

    iets anders

--- day10/day10.py
+++ day10/day10.py
@@ -5,7 +5,8 @@ import matplotlib.animation as animation
 from matplotlib import cm
 import numpy as np
 from celluloid import Camera
-import pylab 
+import pylab
+
 
 def parse_inputfile(inputfile):
     # position=< 9,  1> velocity=< 0,  2>
@@ -17,8 +18,9 @@ def parse_inputfile(inputfile):
         xposition.append(int(m.group(1)))
         yposition.append(int(m.group(2)))
         velocity.append([int(m.group(3)), int(m.group(4))])
-    
+
     return xposition, yposition, velocity
+
 
 def updateposition(xposition, yposition, velocity):
     for i in range(len(xposition)):
@@ -27,39 +29,33 @@ def updateposition(xposition, yposition, velocity):
     return xposition, yposition
 
 
-
-
 if __name__ == "__main__":
-    
-    input_file = "input.txt"
+
+    input_file = "small.txt"
     xposition, yposition, velocity = parse_inputfile(input_file)
 
     boxsizes = []
     for i in range(30000):
         xposition, yposition = updateposition(xposition, yposition, velocity)
-        xmax = max(xposition)
-        xmin = min(xposition) 
-        ymax = max(yposition)
-        ymin = min(yposition) 
-        if xmin < 0:
-            xmax += abs(xmin)
-            xmin += abs(xmin)
-        if ymin < 0:
-            ymin += abs(ymin)
-            ymin += abs(ymin)
-
-        boxsizes.append(xmax*ymax)
+        boxsizes.append(max(xposition) - min(xposition) + max(yposition) - min(yposition))
 
     imin = np.argmin(boxsizes)
-    print("imin: ", imin+1)
+
+    print("Have to wait %s seconds" % (imin + 1))
     xposition, yposition, velocity = parse_inputfile(input_file)
-    for _ in range(imin+1):
-        xposition, yposition = updateposition(xposition, yposition, velocity)
-    
+    # for _ in range(imin + 1):
+    #     xposition, yposition = updateposition(xposition, yposition, velocity)
+
+    for i in range(len(xposition)):
+        xposition[i] = xposition[i] + ((imin+1) * velocity[i][0])
+        yposition[i] = yposition[i] + ((imin+1) * velocity[i][1])
+
+    for i in range(len(xposition)):
+        xposition[i] = xposition[i] 
+        yposition[i] = yposition[i] * -1 # Flip the image
     plt.plot(xposition, yposition, "bo")
     plt.show()
 
-    
 
 # RPNNXFZR
 # 10946
